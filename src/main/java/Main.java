c05310637cf9a0ac11c280a6e18ddca493911214
AnhagaBallas <mickle1212@mail.ru>
2023-05-23 20:54:50 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    static ArrayBlockingQueue<String> arrayBlockingQueue1 = new ArrayBlockingQueue<>(100);
+    static ArrayBlockingQueue<String> arrayBlockingQueue2 = new ArrayBlockingQueue<>(100);
+    static ArrayBlockingQueue<String> arrayBlockingQueue3 = new ArrayBlockingQueue<>(100);
+    static volatile AtomicInteger countA = new AtomicInteger(0);
+    static volatile AtomicInteger countB = new AtomicInteger(0);
+    static volatile AtomicInteger countC = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                arrayBlockingQueue1.add(generateText("abc", 100));
+                arrayBlockingQueue2.add(generateText("abc", 100));
+                arrayBlockingQueue3.add(generateText("abc", 100));
+            }
+
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Thread thread1 = new Thread(() -> {
+            for (String e : arrayBlockingQueue1) {
+                for (int i = 0; i < e.length(); i++) {
+                    if (e.charAt(i) == 'a') {
+                        countA.getAndIncrement();
+                    }
+                }
+
+            }
+
+        });
+        Thread thread2 = new Thread(() -> {
+            for (String e : arrayBlockingQueue2) {
+                for (int i = 0; i < e.length(); i++) {
+                    if (e.charAt(i) == 'b') {
+                        countB.getAndIncrement();
+                    }
+                }
+
+            }
+
+        });
+        Thread thread3 = new Thread(() -> {
+            for (String e : arrayBlockingQueue3) {
+                for (int i = 0; i < e.length(); i++) {
+                    if (e.charAt(i) == 'c') {
+                        countC.getAndIncrement();
+                    }
+                }
+
+            }
+
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Количесво 'a' " + countA);
+        System.out.println("Количесво 'b' " + countB);
+        System.out.println("Количесво 'c' " + countC);
+
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
