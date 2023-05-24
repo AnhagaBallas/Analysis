ee3614221da7e48c3718c44f71d93a558eafdb4f
AnhagaBallas <102907803+AnhagaBallas@users.noreply.github.com>
2023-05-24 20:48:12 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -11,6 +14,9 @@ public class Main {
     static volatile AtomicInteger countC = new AtomicInteger(0);
 
     public static void main(String[] args) {
+        List<Integer> listForA = new ArrayList<>();
+        List<Integer> listForB = new ArrayList<>();
+        List<Integer> listForC = new ArrayList<>();
         Thread thread = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
                 arrayBlockingQueue1.add(generateText("abc", 100));
@@ -30,10 +36,14 @@ public class Main {
                 for (int i = 0; i < e.length(); i++) {
                     if (e.charAt(i) == 'a') {
                         countA.getAndIncrement();
+
                     }
                 }
+                listForA.add(countA.get());
+                countA.getAndSet(0);
 
             }
+            countA.getAndSet(Collections.max(listForA));
 
         });
         Thread thread2 = new Thread(() -> {
@@ -43,8 +53,11 @@ public class Main {
                         countB.getAndIncrement();
                     }
                 }
+                listForB.add(countB.get());
+                countB.getAndSet(0);
 
             }
+            countB.getAndSet(Collections.max(listForB));
 
         });
         Thread thread3 = new Thread(() -> {
@@ -54,9 +67,11 @@ public class Main {
                         countC.getAndIncrement();
                     }
                 }
+                listForC.add(countC.get());
+                countC.getAndSet(0);
 
             }
-
+            countC.getAndSet(Collections.max(listForC));
         });
         thread1.start();
         thread2.start();
@@ -68,9 +83,10 @@ public class Main {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Количесво 'a' " + countA);
-        System.out.println("Количесво 'b' " + countB);
-        System.out.println("Количесво 'c' " + countC);
+        System.out.println("Максимальное количство символа 'a' в строке " + countA);
+        System.out.println("Максимальное количство символа 'b' в строке " + countB);
+        System.out.println("Максимальное количство символа 'c' в строке " + countC);
+
 
     }
 
